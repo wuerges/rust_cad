4b60a7734a0d469ddb245fc4bfb5c20469ebf55a
Emilio Wuerges <wuerges@gmail.com>
2019-11-28 15:25:47 -0300

    starting to get the grips of rust

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,42 +1,75 @@
 
-type Pt = [i32; 3];
+// type Pt = [i32; 3];
+type Pt<T> = [T; 3];
 
-struct Rect {
-    p1 : Pt,
-    p2 : Pt
-}
-
-fn minpt(p1 : Pt, p2 : Pt) -> Pt {
-    let mut r : [i32; 3] = p1;
-    for i in 0..2 {
-        if p2[i] < p1[i] {
-            r[i] = p2[i];
-        }
-    }
-    return r;
-}
-
-fn maxpt(p1 : Pt, p2 : Pt) -> Pt {
-    let mut r : [i32; 3] = p1;
-    for i in 0..2 {
-        if p2[i] > p1[i] {
-            r[i] = p2[i];
-        }
-    }
-    return r;
+struct Rect<T> {
+    p1 : Pt<T>,
+    p2 : Pt<T>
 }
 
 
-fn xsum(a : i32, b :i32) -> i32 {
-    return a + b
+
+fn zip_pt_with<T: Ord + Copy>(p1 : Pt<T>, p2 : Pt<T>, f : &dyn Fn(T, T) -> T) -> Pt<T> {
+    return [f(p1[0], p2[0]), f(p1[1], p2[1]), f(p1[2], p2[2])];
 }
+
+fn minpt<T: Ord + Copy>(p1 : Pt<T>, p2 : Pt<T>) -> Pt<T> {
+    return zip_pt_with(p1, p2, &std::cmp::min);
+}
+
+fn maxpt<T: Ord + Copy>(p1 : Pt<T>, p2 : Pt<T>) -> Pt<T> {
+    return zip_pt_with(p1, p2, &std::cmp::max);
+}
+
+
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, xsum(2,2));
+    
+    use quickcheck::Arbitrary;
+    use quickcheck::Gen;
+    
+    #[derive(Clone, Debug)]
+    struct Pt32(pub Pt<u32>);
+    impl Arbitrary for Pt32 {
+
+        fn arbitrary<G: Gen>(g : &mut G) -> Self {
+            let v : [u32; 3]= [g.next_u32(), g.next_u32(),g.next_u32()];
+            return Pt32(v);
+        }
+    
+    }
+
+    #[quickcheck]
+    fn prop_minpt_is_min(p1 : Pt32, p2: Pt32) -> bool {
+        let p1 = p1.0;
+        let p2 = p2.0;
+
+        let x = minpt(p1, p2);
+
+        return x[0] <= p1[0] && x[1] <= p1[1] && x[2] <= p1[2] && 
+               x[0] <= p2[0] && x[1] <= p2[1] && x[2] <= p2[2] ;
+    }
+
+    #[quickcheck]
+    fn prop_maxpt_is_max(p1 : Pt32, p2: Pt32) -> bool {
+        let p1 = p1.0;
+        let p2 = p2.0;
+
+        let x = maxpt(p1, p2);
+
+        return x[0] >= p1[0] && x[1] >= p1[1] && x[2] >= p1[2] && 
+               x[0] >= p2[0] && x[1] >= p2[1] && x[2] >= p2[2] ;
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+
+//     use super::*;
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, xsum(2,2));
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
 pub mod geometry;
 
