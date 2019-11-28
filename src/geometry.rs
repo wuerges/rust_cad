2b2c379c29aa82c28f6c5a29b19f59a997194ecb
Emilio Wuerges <wuerges@gmail.com>
2019-11-28 13:33:36 -0300

    commiting

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,9 +1,35 @@
 
+type Pt = [i32; 3];
+
+struct Rect {
+    p1 : Pt,
+    p2 : Pt
+}
+
+fn minpt(p1 : Pt, p2 : Pt) -> Pt {
+    let mut r : [i32; 3] = p1;
+    for i in 0..2 {
+        if p2[i] < p1[i] {
+            r[i] = p2[i];
+        }
+    }
+    return r;
+}
+
+fn maxpt(p1 : Pt, p2 : Pt) -> Pt {
+    let mut r : [i32; 3] = p1;
+    for i in 0..2 {
+        if p2[i] > p1[i] {
+            r[i] = p2[i];
+        }
+    }
+    return r;
+}
+
 
 fn xsum(a : i32, b :i32) -> i32 {
     return a + b
 }
-
 
 #[cfg(test)]
 mod tests {
