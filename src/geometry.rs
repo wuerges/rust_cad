ec3f3dc14235bdff8be0af85459565bdac9e39d4
Emilio Wuerges <wuerges@gmail.com>
2019-11-28 16:23:10 -0300

    implemented area, expand and intersection

--- src/geometry.rs
+++ src/geometry.rs
@@ -9,14 +9,44 @@ struct Rect {
 }
 
 impl Rect {
+
+    fn make(p1 : Pt, p2 :Pt) -> Rect {
+        return Rect {
+            p1 : minpt(p1, p2),
+            p2 : maxpt(p1, p2)
+        }
+    }
+
+
     fn expand(&self, e :u32) -> Rect {
-        let p1n = [self.p1[0]-1, self.p1[1]-1, self.p1[2]];
-        let p2n = [self.p2[0]+1, self.p2[1]+1, self.p2[2]];
+        let p1n = [self.p1[0]-e, self.p1[1]-e, self.p1[2]];
+        let p2n = [self.p2[0]+e, self.p2[1]+e, self.p2[2]];
         return Rect { p1 : p1n, p2 : p2n };
+    }
+
+    fn area(&self) -> f64 {
+        return zip_pt_with(self.p1, self.p2, 
+            &|e1,e2| (e1 as f64 - e2 as f64).abs())
+            .iter()
+            .fold(1.0, |m,i| m*i);
+    }
+
+    fn intersection(&self, other : &Rect) -> Option<Rect> {
+
+        let p1i = maxpt(self.p1, other.p1);
+        let p2i = minpt(self.p2, other.p2);
+
+        for i in 0..2 {
+            if p1i[i] > p2i[i] {
+                return None
+            }
+        }
+
+        return Some(Rect::make(p1i, p2i));
     }
 }
 
-fn zip_pt_with(p1 : Pt, p2 : Pt, f : &dyn Fn(u32, u32) -> u32) -> Pt {
+fn zip_pt_with<U : Copy, V : Copy, T>(p1 : [U; 3], p2 : [V; 3], f : &dyn Fn(U, V) -> T) -> [T; 3] {
     return [f(p1[0], p2[0]), f(p1[1], p2[1]), f(p1[2], p2[2])];
 }
 
@@ -69,6 +99,7 @@ mod tests {
         return x[0] >= p1[0] && x[1] >= p1[1] && x[2] >= p1[2] && 
                x[0] >= p2[0] && x[1] >= p2[1] && x[2] >= p2[2] ;
     }
+
 }
 
 // #[cfg(test)]
