78ebc67ebe7ed152d26c00b5b6e534a301f5955d
Emilio Wuerges <wuerges@gmail.com>
2019-12-01 16:42:17 -0300

    not even compiling

--- src/locationfinder.rs
+++ src/locationfinder.rs
@@ -14,11 +14,9 @@ use std::rc::Rc;
 //     path : Option<Vec<Pt>>
 // }
 
-enum Edge {
-    RTQ (Box<RTreeQueue<usize>>), // Represents the neighboors of a vertex
-    Edge { u : usize, v : usize },
-    Route { u: usize, v : usize, length : u32, route : Vec<Pt> }
-}
+// enum Edge {
+struct RTQ { u : usize, rtq : Box<RTreeQueue<usize>> }
+struct Route { u: usize, v : usize, length : u32, route : Vec<Pt> }
 
 struct Finder {
     shape_index : RTree<usize>,
@@ -42,33 +40,48 @@ impl Finder {
     }
 
     fn route(&mut self) -> Vec<Vec<Pt>> {
-        let mut q = PriorityQueue::<u32, usize>::new();
         let mut muf = MUF::new(self.g.vertices.len());
         let mut num_edges = 0;
+        let vertices = &self.g.vertices;
+        
+        let mut rtqs = Vec::<RTQ>::new();
+        let mut routes = Vec::<Route>::new();
+        
+        let mut rtq_q = PriorityQueue::<u32, usize>::new();
+        let mut route_q = PriorityQueue::<u32, usize>::new();
 
-        let mut edges = Vec::<Edge>::new();
-
-        for (u, u_rect) in self.g.vertices.iter().enumerate() {
+        for (u, u_rect) in vertices.iter().enumerate() {
             let mut z = RTreeQueue::new(*u_rect, Rc::clone(&self.shape_index.0));
 
             while z.peek() == 0 {
                 let v = z.pop().unwrap();
-                num_edges += 1;
-                muf.union(u, v);
+                if muf.find(u) != muf.find(v) {
+                    muf.union(u, v);
+                    num_edges += 1;
+                }
             }
 
-            q.push(z.peek(), edges.len());
-            edges.push(Edge::RTQ(Box::new(z)));
+            rtq_q.push(z.peek(), rtqs.len());
+            rtqs.push(RTQ{ u : u, rtq : Box::new(z) });
         }
 
         while num_edges + 1 < self.g.vertices.len() {
-            match edges[q.pop().unwrap().value] {
-                Edge::RTQ(rtq) => {
+            let e = q.pop().unwrap().value;
 
+            match &mut edges[e] {
+                Edge::RTQ {u, rtq} => {
+                    let v = rtq.pop().unwrap();
+                    q.push(rtq.peek(), e);
+                    if muf.find(v) != muf.find(*u) {
+                        let d = vertices[*u].distance(&vertices[v]);
+                        
+                        q.push(d, edges.len());
+                        edges.push(Edge::Route {u : *u, v: v, length : d, route : Vec::new() });
+                    }
                 },
-                Edge::Edge {u, v} => {
+                // Edge::Edge {u, v} => {
 
-                },
+                // },
                 Edge::Route {u, v, length, route} => {
 
                 }
