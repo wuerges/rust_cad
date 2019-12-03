b9823e7d1e7b4dcd1c5084f149d83734146e42a0
Emilio Wuerges <wuerges@gmail.com>
2019-12-02 23:50:29 -0300

    even with normal refs, it is slow'

--- src/locationfinder.rs
+++ src/locationfinder.rs
@@ -6,7 +6,6 @@ use crate::priorityqueue::*;
 use crate::rtreequeue::*;
 use crate::muf::*;
 use crate::astar::*;
-use std::rc::Rc;
 
 struct RTQ<'a> { u : usize, rtq : RTreeQueue<'a, usize> }
 
@@ -36,30 +35,49 @@ impl Finder {
         
         let mut routes = Vec::<Vec::<Pt>>::new();
         
-        let mut rtq_q = PriorityQueue::<u32, Box<RTQ>>::new();
+        // let mut rtq_q = PriorityQueue::<u32, Box<RTQ>>::new();
         let mut route_q = PriorityQueue::<u32, (usize, usize, Route)>::new();
 
-        for (u, u_rect) in vertices.iter().enumerate() {
-            println!("progress {:?} {:?} {:?}/{:?}", u, u_rect, num_edges+1, vertices.len());
+        let mut rtq_vec : Vec<_> = vertices.iter().enumerate().map( |(u, u_rect)| {
+            RTQ{ u : u, rtq : RTreeQueue::new(*u_rect, &self.shape_index)}
+        }).collect();
 
-            // let z = RTreeQueue::new(*u_rect, Rc::clone(&self.shape_index.0));
-            // let mut z = ;
-
-            let mut q = Box::new(RTQ{ u, rtq : RTreeQueue::new(*u_rect, &self.shape_index) });
-
-            // while q.rtq.peek() == 0 {
-
-            //     let v = q.rtq.pop().unwrap();
-            //     println!("u={:?}, v={:?}, r_u={:?}, r_v={:?} dist={:?}", u, v, vertices[u], vertices[v], vertices[u].distance(&vertices[v]));
-            //     if muf.find(u) != muf.find(v) {
-            //         muf.union(u, v);
-            //         num_edges += 1;
-            //     }
-            // }
-
-            let q_key = q.rtq.peek();
-            rtq_q.push(q_key, q);
+        let mut rtq_q = PriorityQueue::<u32, &mut RTQ>::new();
+        for q in &mut rtq_vec {
+            while q.rtq.peek() == 0 {
+                let v = q.rtq.pop().unwrap();
+                if muf.find(q.u) != muf.find(v) {
+                    muf.union(q.u, v);
+                    num_edges += 1;
+                }
+            }
+            rtq_q.push(q.rtq.peek(), q);
         }
+
+
+
+
+        // for (u, u_rect) in vertices.iter().enumerate() {
+        //     println!("progress {:?} {:?} {:?}/{:?}", u, u_rect, num_edges+1, vertices.len());
+
+        //     // let z = RTreeQueue::new(*u_rect, Rc::clone(&self.shape_index.0));
+        //     // let mut z = ;
+
+        //     let mut q = Box::new(RTQ{ u, rtq : RTreeQueue::new(*u_rect, &self.shape_index) });
+
+        //     while q.rtq.peek() == 0 {
+
+        //         let v = q.rtq.pop().unwrap();
+        //         // println!("u={:?}, v={:?}, r_u={:?}, r_v={:?} dist={:?}", u, v, vertices[u], vertices[v], vertices[u].distance(&vertices[v]));
+        //         if muf.find(u) != muf.find(v) {
+        //             muf.union(u, v);
+        //             num_edges += 1;
+        //         }
+        //     }
+
+        //     let q_key = q.rtq.peek();
+        //     rtq_q.push(q_key, q);
+        // }
 
         while num_edges + 1 < vertices.len() {
             if num_edges % 100 == 0 {
